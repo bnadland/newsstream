ed54376ac31b559cbd94349aed03652030af0b0c
Benjamin Nadland <me@benjaminnadland.de>
2015-08-31 08:48:54 +0200

    Update

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"github.com/caser/gophernews"
 	"github.com/jzelinskie/geddit"
+	"io/ioutil"
+	"net/http"
 	//	"strconv"
 	//	"github.com/jinzhu/now"
 	"github.com/PuerkitoBio/purell"
@@ -17,13 +20,20 @@ type NewsItem struct {
 	Title       string
 	Url         string
 	Source      string
-	Body        string `sql:"type:text"`
+	Raw         string `sql:"type:text"`
+	HasRaw      bool
+	Error       bool
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 	PublishedAt time.Time
 }
 
 func saveNewsItem(db *gorm.DB, newsItem NewsItem) error {
+	defer func() {
+		if r := recover(); r != nil {
+			log.WithFields(log.Fields{"func": "saveNewsItem", "r": r}).Error("Recovered")
+		}
+	}()
 	url, err := purell.NormalizeURLString(newsItem.Url, purell.FlagsSafe)
 	if err != nil {
 		log.WithFields(log.Fields{"func": "saveNewsItem", "err": err}).Error("Url normalization failed")
@@ -42,7 +52,7 @@ func saveNewsItem(db *gorm.DB, newsItem NewsItem) error {
 			"Title":       newsItem.Title,
 			"Url":         newsItem.Url,
 			"PublishedAt": newsItem.PublishedAt,
-		}).Info("Saving newsItem")
+		}).Info("New NewsItem")
 
 		db.Save(&newsItem)
 	}
@@ -53,7 +63,8 @@ func saveNewsItem(db *gorm.DB, newsItem NewsItem) error {
 func crawlSubreddit(db *gorm.DB, subreddit string) error {
 	r := geddit.NewSession("newsstream")
 
-	submissions, err := r.SubredditSubmissions(subreddit)
+	submissions, err := r.SubredditSubmissions(subreddit, geddit.NewSubmissions, geddit.ListingOptions{
+		Limit: 100})
 	if err != nil {
 		log.WithFields(log.Fields{"func": "main", "err": err, "subreddit": subreddit}).Error("Could not get submissions")
 		return err
@@ -101,19 +112,68 @@ func crawlHackernews(db *gorm.DB) error {
 	return nil
 }
 
+func getRawBodyFromUrl(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.WithFields(log.Fields{"func": "getRawBodyFromUrl", "url": url, "err": err}).Error("Could not fetch url")
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.WithFields(log.Fields{"func": "getRawBodyFromUrl", "url": url, "err": err}).Error("Could read response")
+		return "", err
+	}
+
+	return fmt.Sprintf("%s", body), nil
+}
+
+func crawlRawNewsItems(db *gorm.DB) error {
+	for {
+		newsItem := &NewsItem{}
+		if db.Where("raw=''").Where("error = false").First(newsItem).RecordNotFound() {
+			log.WithFields(log.Fields{"func": "crawlRawNewsItems"}).Info("No new NewsItems")
+			time.Sleep(10 * time.Second)
+			continue
+		}
+		body, err := getRawBodyFromUrl(newsItem.Url)
+		if err != nil {
+			log.WithFields(log.Fields{"func": "crawlRawNewsItems", "err": err, "url": newsItem.Url}).Info("Could not get raw body from url")
+			newsItem.Error = true
+			db.Save(newsItem)
+			continue
+		}
+
+		log.WithFields(log.Fields{
+			"func": "crawlRawNewsItem",
+			"Url":  newsItem.Url,
+		}).Info("Got raw body")
+
+		newsItem.Raw = body
+		newsItem.Error = false
+		db.Save(newsItem)
+		time.Sleep(1 * time.Second)
+	}
+}
+
 func main() {
 	db, err := gorm.Open("postgres", "postgres://newsstream:newsstream@127.0.0.1?sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
-	db.DropTable(&NewsItem{})
-	db.LogMode(true)
+	//db.DropTable(&NewsItem{})
+	//db.LogMode(true)
 
 	db.AutoMigrate(&NewsItem{})
 
-	for _, subreddit := range []string{"golang", "python", "worldnews"} {
-		crawlSubreddit(&db, subreddit)
-	}
-	//crawlHackernews(db)
+	/*
+		for _, subreddit := range []string{"golang", "python", "worldnews"} {
+			crawlSubreddit(&db, subreddit)
+		}
+	*/
+	/*
+		crawlHackernews(&db)
+	*/
+	crawlRawNewsItems(&db)
 }
